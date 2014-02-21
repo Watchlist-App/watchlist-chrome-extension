56fc232c1b87d69856893255a8a071204c5361f6
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-02-21 23:22:53 +0300

    injected simple button into google.com description box

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,20 @@
 'use strict';
+
+var button = document.createElement('input');
+button.type = 'button';
+button.value = 'Add to watchlist';
+
+button.onclick = function(){
+  console.log('adding to watchlist');
+};
+
+var waitForBox = setInterval(checkForElement, 150);
+
+function checkForElement() {
+  var box = document.querySelector('.kno-ecr-pt');
+  if (box) {
+    clearInterval (waitForBox);
+    box.appendChild(button);
+  }
+}
+
