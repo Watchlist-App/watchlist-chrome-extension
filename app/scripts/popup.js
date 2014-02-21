e6bae83968f21c75d767f0da0d41fdd40833d8c8
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-02-22 01:05:05 +0300

    added simple movie lists popup

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,1 +1,27 @@
 'use strict';
+
+var renderList = function(list){
+  var ul = document.createElement('ul');
+  var container = document.getElementById('container');
+  container.appendChild(ul);
+  list.forEach(function(element){
+    var li = document.createElement('li');
+    li.innerHTML = element.title;
+    ul.appendChild(li);
+  });
+}
+
+var request = new XMLHttpRequest;
+request.open('GET', 'http://watchlist-koa.herokuapp.com/user/529dfc93d6bb28020079268d', true);
+
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400){
+    var user = JSON.parse(request.responseText);
+    renderList(user.lists);
+  } else {
+    console.log("error");
+  }
+};
+
+request.send();
+
