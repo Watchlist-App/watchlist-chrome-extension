301bf3eaeff80ae540c68deccf2dc4e41a2b12c4
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-03-02 20:21:06 +0300

    refactoring

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,15 +1,10 @@
 'use strict';
 
+var userId = '53134ae78b42c302008eed55';
+
 var ul = document.createElement('ul');
 var container = document.getElementById('container');
 container.appendChild(ul);
-
-var fetchWatchlist = function(response){
-  var watchlist = response.lists.filter(function(list){return list.title == "watchlist"})[0];
-  watchlist.movies.map(function(movieID){
-    makeRequest('http://watchlist-koa.herokuapp.com/tmdb/3/movie/' + movieID, renderMovie);
-  });
-}
 
 var renderMovie = function(movie){
   var li = document.createElement('li');
@@ -20,10 +15,19 @@ var renderMovie = function(movie){
   poster.src = 'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w92/' + movie.poster_path;
   li.appendChild(poster);
   li.appendChild(title);
-  li.onclick = function () {
+  li.addEventListener('click', function () {
     chrome.tabs.create({active: true, url: link});
-  };
+  });
   ul.appendChild(li);
-}
+};
 
-makeRequest('http://watchlist-koa.herokuapp.com/user/529dfc93d6bb28020079268d', fetchWatchlist);
+var fetchWatchlist = function(user){
+  var watchlist = user.lists.filter(function(list){
+    return list.title === 'watchlist';
+  })[0];
+  watchlist.movies.map(function(movieID){
+    makeRequest('http://watchlist-koa.herokuapp.com/tmdb/3/movie/' + movieID, renderMovie);
+  });
+};
+
+fetchUser(userId, fetchWatchlist);
