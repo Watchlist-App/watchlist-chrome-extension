864512f9d0879b8438012e8a7ea0720436b535bd
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-03-02 18:18:43 +0300

    implemented basic watchlist requests

--- app/scripts/googleContentScript.js
+++ app/scripts/googleContentScript.js
@@ -1,24 +1,35 @@
 'use strict';
 
 var movie = {};
+var userId = '529ed16dc7e98b020066f901';
 
-var button = document.createElement('input');
-button.type = 'button';
-button.value = 'Add to watchlist';
-button.className = 'watchlist-btn-google';
+var createButton = function(){
+  var button = document.createElement('input');
+  button.type = 'button';
+  button.value = 'Add to watchlist';
+  button.className = 'watchlist-btn-google';
+  button.addEventListener('click', function(){
+    addMovieToWatchlist(userId, movie.id, function(response){
+      console.log(movie.id);
+      button.value = 'in watchlist';
+      console.log(response);
+    });
+  });
+  return button;
+};
 
-button.addEventListener('click', function(){
-  console.log('adding ' + movie.title + ' year:' + movie.year + ' to watchlist');
-});
-
-document.addEventListener("DOMNodeInserted", function(e) {
-  if (e.target.id == "rhs"){
+document.addEventListener('DOMNodeInserted', function(e) {
+  if (e.target.id === 'rhs'){
     var movieTitleDiv = e.target.querySelector('.kno-ecr-pt');
     var caption = e.target.querySelector('._ps');
     if (caption.innerHTML.match(/Film/)){
       movie.title = movieTitleDiv.innerHTML;
       movie.year = caption.innerHTML.match(/(\d+)/)[0];
-      movieTitleDiv.appendChild(button);
+      findMovie(movie.title, movie.year, function(response){
+        movie.id = response.id;
+        var button = createButton();
+        movieTitleDiv.appendChild(button);
+      });
     }
   }
 }, false);
--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var makeRequest = function(url, cb){
-  var request = new XMLHttpRequest;
+  var request = new XMLHttpRequest();
   request.open('GET', url, true);
 
   request.onload = function() {
@@ -9,9 +9,33 @@ var makeRequest = function(url, cb){
       var response = JSON.parse(request.responseText);
       cb(response);
     } else {
-      console.log("error");
+      console.log('error');
     }
   };
 
   request.send();
-}
+};
+
+var addMovieToWatchlist = function(userId, movieId, cb){
+  var url = 'http://watchlist-app-server.herokuapp.com/list/addMovie?listTitle=watchlist&userId=' + userId + '&movieId=' + movieId;
+  makeRequest(url, function(response){
+    cb(response);
+  });
+};
+
+var findMovie = function(title, year, cb){
+  var url = 'http://watchlist-koa.herokuapp.com/tmdb/3/search/movie?query='+ title + '&year=' + year;
+  makeRequest(url, function(response){
+    cb(response.results[0]);
+  });
+};
+
+var getWatchlist = function(userId, cb){
+  var url = 'http://watchlist-koa.herokuapp.com/user/' + userId;
+  makeRequest(url, function(response){
+    var watchlist = response.lists.filter(function(list){
+      return list.title === 'watchlist';
+    })[0];
+    cb(watchlist);
+  });
+};
