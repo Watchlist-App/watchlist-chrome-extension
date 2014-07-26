3f3279d3c526628f6853b6aefffbf6c4a1299bf1
Evgeny Traytyak <evgen.traytyak@gmail.com>
2014-07-26 20:38:11 +0300

    Refactoring imdbContentScript.js

--- app/scripts/imdbContentScript.js
+++ app/scripts/imdbContentScript.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var userId = '53134ae78b42c302008eed55';
+var userId = '53134ae78b42c302008eed55',
+	box = document.querySelector('#prometer_container'),
+	movieMetadata = document.querySelector('title').innerHTML.match(/^(.+)\s\((\d+)\)/),
+	movieTitle = movieMetadata[1],
+	movieYear = movieMetadata[2];
 
-var box = document.querySelector('#prometer_container');
-var movieMetadata = document.querySelector('title').innerHTML.match(/^(.+)\s\((\d+)\)/);
-var movieTitle = movieMetadata[1];
-var movieYear = movieMetadata[2];
-
-findMovie(movieTitle, movieYear, function(movie){
-  fetchUser(userId, function(user){
-    var button = createButton(user, movie.id);
-    box.appendChild(button);
-  });
+findMovie(movieTitle, movieYear, function(movie) {
+	fetchUser(userId, function(user) {
+		var button = createButton(user, movie.id);
+		box.appendChild(button);
+	});
 });
