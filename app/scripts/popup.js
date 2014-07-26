1230885324af5feac50fa7cd67e27868ddfe48e2
Evgeny Traytyak <evgen.traytyak@gmail.com>
2014-07-26 15:44:05 +0300

    Refactoring popup.js

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,33 +1,34 @@
 'use strict';
 
-var userId = '53134ae78b42c302008eed55';
+var renderMovie, fetchWatchlist,
+   userId = '53134ae78b42c302008eed55',
+   ul = document.createElement('ul'),
+   container = document.getElementById('container');
 
-var ul = document.createElement('ul');
-var container = document.getElementById('container');
-container.appendChild(ul);
+renderMovie = function(movie) {
+   var li = document.createElement('li'),
+      title = document.createElement('h4'),
+      poster = document.createElement('img'),
+      link = 'http://watchlist-webapp.herokuapp.com/#/movie/' + movie.id;
 
-var renderMovie = function(movie){
-  var li = document.createElement('li');
-  var title = document.createElement('h4');
-  title.innerHTML = movie.title;
-  var poster = document.createElement('img');
-  var link = 'http://watchlist-webapp.herokuapp.com/#/movie/' + movie.id;
-  poster.src = 'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w92/' + movie.poster_path;
-  li.appendChild(poster);
-  li.appendChild(title);
-  li.addEventListener('click', function () {
-    chrome.tabs.create({active: true, url: link});
-  });
-  ul.appendChild(li);
+   container.appendChild(ul);
+   title.innerHTML = movie.title;
+   poster.src = 'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w92/' + movie.poster_path;
+   li.appendChild(poster);
+   li.appendChild(title);
+   li.addEventListener('click', function() {
+      chrome.tabs.create({active: true, url: link});
+   });
+   ul.appendChild(li);
 };
 
-var fetchWatchlist = function(user){
-  var watchlist = user.lists.filter(function(list){
-    return list.title === 'watchlist';
-  })[0];
-  watchlist.movies.map(function(movieID){
-    makeRequest('http://watchlist-koa.herokuapp.com/tmdb/3/movie/' + movieID, renderMovie);
-  });
+fetchWatchlist = function(user) {
+   var watchlist = user.lists.filter(function(list) {
+      return list.title === 'watchlist';
+   })[0];
+   watchlist.movies.map(function(movieID) {
+      makeRequest('http://watchlist-koa.herokuapp.com/tmdb/3/movie/' + movieID, renderMovie);
+   });
 };
 
 fetchUser(userId, fetchWatchlist);
